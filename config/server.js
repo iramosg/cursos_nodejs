11dd4436bbb35345e1fc98c3ca535c802550daaa
Jay Benedicto <Jay@MacBook-Pro-de-Jay.local>
2018-03-02 07:17:05 -0300

    cosign que cria as rotas baseado nos arquivos da pasta

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 var express = require('express'); //retorna uma funcao
+var consign = require('consign'); //retorna uma funcao
+
 var app = express(); //executa a funcao dentro do express
 app.set('view engine', 'ejs');// view engine do node.js
 app.set('views', './app/views');//aponta o caminho das views
+
+consign().include('./app/routes').into(app);
 
 module.exports = app;
