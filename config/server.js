12f91e30e79c3acc635cf25b3a4ddd480d583b24
Jaqueline Benedicto <soamazing@gmail.com>
2018-07-10 18:34:31 -0300

    mais informações detalhadas

--- config/server.js
+++ config/server.js
@@ -5,6 +5,7 @@ var app = express(); //executa a funcao dentro do express
 app.set('view engine', 'ejs');// view engine do node.js
 app.set('views', './app/views');//aponta o caminho das views padrão
 
-consign().include('./app/routes').into(app); //inclui todos os arquivos de rota 
+//tem que ser feito depois do express pois eu preciso inserir o load do consign para dentro do express
+consign().include('./app/routes').into(app); //inclui todos os arquivos de rota  automaticamente
 
 module.exports = app;
