2f96200ffe92e6d3c4994456d39ef1f6ba13e8e8
Jaqueline Benedicto <soamazing@gmail.com>
2018-07-11 22:32:18 -0300

    gravando no banco

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 var express = require('express'); //retorna uma funcao
 var consign = require('consign'); //retorna uma funcao
+var bodyParser = require('body-parser'); //para receber via post as informações
 
 var app = express(); //executa a funcao dentro do express
 app.set('view engine', 'ejs');// view engine do node.js
 app.set('views', './app/views');//aponta o caminho das views padrão
+
+app.use(bodyParser.urlencoded({extended: true})); //codifica a url
 
 //tem que ser feito depois do express pois eu preciso inserir o load do consign para dentro do express
 //toda vez que a gente chama um módulo no consign ele já EXECUTA a função, para ele não fazer isso
